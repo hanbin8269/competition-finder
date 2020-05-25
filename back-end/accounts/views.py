704cebbc2e1d12bcbf04783a8412d42f5811fe92
hanbin8269 <hanbin8269@gmail.com>
2020-05-26 03:10:33 +0900

    [CREATE] urls, LoginView

--- back-end/accounts/views.py
+++ back-end/accounts/views.py
@@ -3,7 +3,7 @@ from django.views import View
 from django.http import JsonResponse
 from .models import User
 
-class SignUpView(View):
+class JoinView(View):
     def post(self, request):
         data = json.loads(request.body)
         User(
@@ -14,7 +14,15 @@ class SignUpView(View):
 
         return JsonResponse({'message' : '회원가입 완료'},status = 200)
 
-class SignInView(View):
+class LoginView(View):
     def post(self, request):
         data = json.loads(request.body)
+        if User.objects.filter(email = data['email']).exists():
+            user = User.objects.get(email = data['email'])
+            if user.password == data['password']:
+                return JsonResponse({'message' : f'{user.email}님 로그인 성공!'},status = 200)
+            else :
+                return JsonResponse({'message' : '비밀번호가 틀렸어요'},status = 200)
+        return JsonResponse({'message' : '등록되지 않은 이메일 입니다.'},status = 200)
+            
         
